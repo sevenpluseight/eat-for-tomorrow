783d10f4c002ecaf1abcc43a153beb024fb3dd06
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-08-22 18:21:39 +0800

    Project setup

---
+++ js/data.js
@@ -0,0 +1,479 @@
+
+const days = [
+  {
+    day: 1,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 2,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 3,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 4,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 5,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 6,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 7,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 8,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 9,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 10,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 11,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 12,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 13,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+  {
+    day: 14,
+    question: "",
+    choices: [
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      },
+      {
+        emoji: "",
+        label: "",
+        health: 0,
+        wallet: 0,
+        env: 0
+      }
+    ]
+  },
+]  
